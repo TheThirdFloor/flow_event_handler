ed84704047b898262e8b5a25e6003ebb575cf1d5
jacobmed <jacobmed@thethirdfloorinc.com>
2024-03-27 17:38:32 -0700

    Plugin rename and setting up configs

--- src/plugins/task_status_to_version_status.py
+++ src/plugins/task_status_to_version_status.py
@@ -6,20 +6,28 @@ Ported from TheThirdFloor/pipeline at tag 5.24.1
 """
 
 
-SCRIPT_NAME = ''
-SCRIPT_KEY  = ''
-SERVER_URL  = ''
+import logging
+import handler_config
+
+PLUGIN_NAME = 'task_status_to_version_status'
 
 
 def registerCallbacks(reg):
-    matchEvents = {
+    config = handler_config.Config(handler_config.getConfigPath())
+    script_name = config.get_api_script_name(PLUGIN_NAME)
+    script_key = config.get_api_script_key(PLUGIN_NAME)
+
+    # Register the event to the daemon
+    event_filter = {
         'Shotgun_Version_Change': ['sg_status_list'],
     }
-    
-    reg.registerCallback(SCRIPT_NAME, SCRIPT_KEY, flipTask, matchEvents, None)
+
+    # Register the event to the daemon
+    reg.registerCallback(script_name, script_key,
+                         task_status_to_version_status, event_filter, None)
 
 
-def flipTask(sg, logger, event, args):
+def task_status_to_version_status(sg, logger, event, args):
     """Flip downstream Tasks to 'rdy' if all of their upstream Tasks are 'cmpt'"""
     
     # Check that the event is linked to something we can process
@@ -63,41 +71,43 @@ def flipTask(sg, logger, event, args):
         sg.update('Task', task['id'], {'sg_status_list': new_value})
 
 
-if __name__ == "__main__":
-    import logging
+def test():
     from shotgun_api3 import Shotgun
-    
+
     logging.basicConfig()
-    logger = logging.getLogger("status_flip_task_from_version")
+    logger = logging.getLogger(PLUGIN_NAME)
     logger.setLevel(logging.DEBUG)
 
-    sg = Shotgun(SERVER_URL, SCRIPT_NAME, SCRIPT_KEY)
-    # Find a relevant event
-    # Version
-    eventsIDs = [3121814, 3122028] # Version
-    
-    events = []
-    
-    for eventID in eventsIDs:
-        filters = [
-                 ['id', 'is', eventID]
-                 ]
-        fields = ['entity', 
-                  'user', 
-                  'meta', 
-                  'project', 
-                  'attribute_name',
-                  'event_type',
-                  'created_at',
-                  'user',
-                  'session_uuid',
-                  'type',
-                  ]  
-        event = sg.find_one('EventLogEntry', filters, fields)
-        events.append(event)
-    
-    for event in events:
-        flipTask(sg, logger, event, None)
+    config = handler_config.Config(handler_config.getConfigPath())
+    server_url = config.server_url
+    script_name = config.get_api_script_name(PLUGIN_NAME)
+    script_key = config.get_api_script_key(PLUGIN_NAME)
+
+    sg = Shotgun(server_url, script_name, script_key)
+
+    # A single event
+    event_id = 10870189
+
+    filters = [
+        ['id', 'is', event_id]
+    ]
+    fields = ['entity',
+              'user',
+              'meta',
+              'project',
+              'attribute_name',
+              'event_type',
+              'created_at',
+              'user',
+              'session_uuid',
+              'type',
+              ]
+
+    event = sg.find_one('EventLogEntry', filters, fields)
+    task_status_to_version_status(sg, logger, event, None)
+
+if __name__ == "__main__":
+    test()
         
         
         
--- src/plugins/user_cleanup.py
+++ src/plugins/user_cleanup.py
@@ -6,23 +6,28 @@ Ported from TheThirdFloor/pipeline at tag 5.24.1
 """
 
 
-SCRIPT_NAME = ''
-SCRIPT_KEY  = ''
-SERVER_URL  = ''
+import logging
+import handler_config
+
+PLUGIN_NAME = 'user_cleanup'
 
 
 def registerCallbacks(reg):
+    config = handler_config.Config(handler_config.getConfigPath())
+    script_name = config.get_api_script_name(PLUGIN_NAME)
+    script_key = config.get_api_script_key(PLUGIN_NAME)
+
     eventsFilter = {
         'Shotgun_HumanUser_Change': ['sg_status_list']
     }
     
     # Register the event to the daemon
-    reg.registerCallback(SCRIPT_NAME, SCRIPT_KEY, cleanup_user, \
+    reg.registerCallback(script_name, script_key, user_cleanup,
                          eventsFilter, None)
     reg.logger.setLevel(logging.INFO)
 
 
-def cleanup_user(sg, logger, event, args):
+def user_cleanup(sg, logger, event, args):
 
     if not event['entity']:
         return
@@ -45,27 +50,30 @@ def cleanup_user(sg, logger, event, args):
     sg.update('HumanUser', user['id'], data)
 
 
-if __name__ == "__main__":
-
-    import logging
+def test():
     from shotgun_api3 import Shotgun
-    
+
     logging.basicConfig()
-    logger = logging.getLogger("user_cleanup")
+    logger = logging.getLogger(PLUGIN_NAME)
     logger.setLevel(logging.DEBUG)
-    
-    sg = Shotgun(SERVER_URL, SCRIPT_NAME, SCRIPT_KEY)
-    
+
+    config = handler_config.Config(handler_config.getConfigPath())
+    server_url = config.server_url
+    script_name = config.get_api_script_name(PLUGIN_NAME)
+    script_key = config.get_api_script_key(PLUGIN_NAME)
+
+    sg = Shotgun(server_url, script_name, script_key)
+
     # A single event
-    event_id = 7926994
+    event_id = 10870149
 
     filters = [
-             ['id', 'is', event_id]
-             ]
-    fields = ['entity', 
-              'user', 
-              'meta', 
-              'project', 
+        ['id', 'is', event_id]
+    ]
+    fields = ['entity',
+              'user',
+              'meta',
+              'project',
               'attribute_name',
               'event_type',
               'created_at',
@@ -73,6 +81,9 @@ if __name__ == "__main__":
               'session_uuid',
               'type',
               ]
-    
+
     event = sg.find_one('EventLogEntry', filters, fields)
-    cleanup_user(sg, logger, event, None)
+    user_cleanup(sg, logger, event, None)
+
+if __name__ == "__main__":
+    test()
